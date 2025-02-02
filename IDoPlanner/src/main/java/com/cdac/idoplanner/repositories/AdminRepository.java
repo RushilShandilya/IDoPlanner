f4465015013ff81a3dafbd374bf1f6c9713511bc
RushilShandilya <rushilcujoh@gmail.com>
2025-02-02 18:24:11 +0530

    Commit

--- IDoPlanner/src/main/java/com/cdac/idoplanner/repositories/AdminRepository.java
+++ IDoPlanner/src/main/java/com/cdac/idoplanner/repositories/AdminRepository.java
@@ -2,7 +2,9 @@ package com.cdac.idoplanner.repositories;
 
 import com.cdac.idoplanner.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
      Admin getByAdminId(Integer adminId);
 }
