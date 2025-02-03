46979ab176a1645743f5a5edf4fd3dee01d0f196
RushilShandilya <rushilcujoh@gmail.com>
2025-02-03 19:04:03 +0530

    Commit

--- IDoPlanner/src/main/java/com/cdac/idoplanner/IDoPlannerApplication.java
+++ IDoPlanner/src/main/java/com/cdac/idoplanner/IDoPlannerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IDoPlannerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IDoPlannerApplication.class, args);
 	}
