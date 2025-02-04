d28664b48a6c1f156974ca7c4b5e2f289f6c262c
Shreya <shreyasingh85301@gmail.com>
2025-02-05 03:29:42 +0530

    add frontend folder to the IdoPlanner for the user interface add assests folder for styles and images add js folder for logic of entire application like auth.js, app.js, dashboard.js, guest.js, planner.js, serviceProvider.js add component folder for resusable components like navbar and footer add pages for various pages in app routing

---
+++ frontend/assets/js/planner.js
@@ -0,0 +1,11 @@
+function manageWedding() {
+  alert("Redirecting to Wedding Management Page...");
+}
+
+function inviteGuests() {
+  alert("Redirecting to Guest Invitation Page...");
+}
+
+function assignVendors() {
+  alert("Redirecting to Vendor Management Page...");
+}
